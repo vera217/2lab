20ceefba1d0a7ab5308e71accc6c127a14fec122
vera217 <vera.serykh.05@bk.ru>
2024-10-19 22:55:55 +0700

    Добавление нового объекта - Sapplier

---
+++ 2lab/Sapplier.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2lab
+{
+    public class Supplier
+    {
+        public string Name { get; set; }
+        public string ContactPerson { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public Supplier(string name, string contactPerson, string phoneNumber)
+        {
+            Name = name;
+            ContactPerson = contactPerson;
+            PhoneNumber = phoneNumber;
+        }
+
+        public string PrintInfo() => $"Поставщик: {Name}\nКонтактное лицо: {ContactPerson}\nТелефон: {PhoneNumber}\n";
+    }
+}
