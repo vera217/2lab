73f38160b632aca8ce5316f69129819f43eeb717
vera217 <vera.serykh.05@bk.ru>
2024-10-19 22:41:06 +0700

    Добавьте файлы проекта.

---
+++ 2lab/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2lab
+{
+    public class Product
+    {
+        public DateTime Date { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+
+        public Product(DateTime date, string name, int quantity)
+        {
+            Date = date;
+            Name = name;
+            Quantity = quantity;
+
+        }
+
+        public virtual string PrintInfo()
+        {
+            return $"Дата: {Date.ToShortDateString()} \nНазвание: {Name} \nКоличество: {Quantity}";
+        }
+    }
+}
---
+++ 2lab/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Globalization;
+using System;
+using _2lab;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string s = "Поступление товаров: 2023.01.05; 'яблоки'; 20 ;";
+        Console.WriteLine(s);
+
+        if (!s.StartsWith("Поступление товаров:"))
+        {
+            Console.WriteLine("Некорректный ввод: строка должна начинаться с Поступление товаров:");
+            return;
+        }
+
+        s = s.Substring(s.IndexOf(":") + 1).Trim();
+
+        string[] words = s.Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+
+
+        if (words.Length < 3)
+        {
+            Console.WriteLine("Некорректные данные: недостаточно элементов.");
+            return;
+        }
+
+        if (!DateTime.TryParseExact(words[0].Trim(), "yyyy.MM.dd", null, DateTimeStyles.None, out DateTime parsedDate))
+        {
+            Console.WriteLine("Неверный ввод даты");
+            return;
+        }
+
+        string productName = words[1].Trim(' ', '\'');
+
+
+        if (int.TryParse(words[2].Trim(), out int quantity))
+        {
+            Product product = new Product(parsedDate, productName, quantity);
+            Console.WriteLine(product.PrintInfo());
+        }
+        else
+        {
+            Console.WriteLine("Некорректные данные");
+        }
+    }
+}
